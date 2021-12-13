87ca5a60cacd0b260f1b9828ed83e02e47b348ff
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-12-13 17:35:36 -0300

    Criação do DefaultHeader

---
+++ src/pages/TelaInicialMinhasCompeticoes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DefaultHeader from "../../components/DefaultHeader";
+
+function TelaInicialMinhasCompeticoes(){
+  return (
+    <div id="page-minhas-competicoes">
+      <DefaultHeader/>
+      <h1>Minhas competições</h1>
+    </div>
+  );
+}
+
+export default TelaInicialMinhasCompeticoes;
