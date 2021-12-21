b911babaac5251435c9253196281c97ade9062cf
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2021-12-20 21:12:34 -0300

    Atualização das imagens para SVG

--- src/components/DefaultHeader/index.js
+++ src/components/DefaultHeader/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import ImagemLogo from '../../assets/images/Imagem1.png';
-import Dropdown from '../../assets/images/Dropdown.png';
-import Grupo from '../../assets/images/Grupo.png';
-import Trofeu from '../../assets/images/Trofeu.png';
-import Confirma from '../../assets/images/Confirma.png';
+import ImagemLogo from '../../assets/images/Imagem1.svg';
+import Dropdown from '../../assets/images/Dropdown.svg';
+import Grupo from '../../assets/images/Grupo.svg';
+import Trofeu from '../../assets/images/Trofeu.svg';
+import Confirma from '../../assets/images/Confirma.svg';
 
 function DefaultHeader() {
   return (
     <div id="component-defaultHeader">
       <div className="bg-warning navbar navbar-light text-white pb-0 pt-0">
-        <div className="container-fluid">
+        <div className="container-fluid ">
 
           <img
             src={ImagemLogo}
@@ -20,9 +20,9 @@ function DefaultHeader() {
             width="150"
           />
 
-          <ul className="nav nav-pills justify-content-center">
+          <ul className="nav nav-pills justify-content-center ">
 
-            <li className="nav-item bg-light">
+            <li className="nav-item bg-light mt-0">
               <a
                 class="nav-link active bg-light p-0"
                 aria-current="page"
@@ -97,35 +97,39 @@ function DefaultHeader() {
 
           </ul>
 
-          <div className="dropdown">
-            <h6 className="ms-0 align-self-center">nome do usuário</h6>
-            <img
-              src={Dropdown}
-              alt="imagem_logo_2"
-              className="img-fluid dropdown-toggle m-3"
-              height="70"
-              width="70"
-              id="navbarDropdownMenu"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            />
+          <div className="row">
+            <div className="col">
+              <h6 className="ms-3 align-middle">nome do usuário</h6>
+            </div>
+            <div className="col dropdown ">
+              <img
+                src={Dropdown}
+                alt="imagem_logo_2"
+                className="img-fluid dropdown-toggle m-3 "
+                height="70"
+                width="70"
+                id="navbarDropdownMenu"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              />
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenu">
+                <li>
+                  <a href="#" className="nav-link text-dark">
+                    <h6>Meus dados</h6>
+                  </a>
+                </li>
 
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenu">
-              <li>
-                <a href="#" className="nav-link text-dark">
-                  <h6>Meus dados</h6>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" className="nav-link text-dark pt-0">
-                  <h6>Sair</h6>
-                </a>
-              </li>
-            </ul>
-
+                <li>
+                  <a href="#" className="nav-link text-dark pt-0">
+                    <h6>Sair</h6>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
+
+
         </div>
       </div>
     </div>
