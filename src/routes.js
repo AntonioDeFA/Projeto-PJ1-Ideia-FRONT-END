3f822a98cf00981f4cdb11ed17f1065e06dfd91a
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-06 02:02:02 -0300

    Rotas atualizadas para novas telas

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,20 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
+import LoginToken from "./pages/LoginToken";
 import TelaInicial from "./pages/TelaInicial";
 import CadastroUsuario from "./pages/CadastroUsuario";
-import LoginToken from "./pages/LoginToken";
+import ResetarSenha from "./pages/ResetarSenha/index";
 
 function ProjetoRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<TelaInicial />} />
+        <Route path="/" element={<TelaInicial />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<CadastroUsuario />} />
         <Route path="/login-token" element={<LoginToken />} />
+        <Route path="/resetar-senha" element={<ResetarSenha />} />
       </Routes>
     </BrowserRouter>
   );
