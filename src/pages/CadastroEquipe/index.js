73302879c9f41dcead78717d149bec36eaa185d0
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-06 22:46:38 -0300

    Inicio de tela p/ cadastro de equipe

---
+++ src/pages/CadastroEquipe/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import DefaultHeader from "../../components/DefaultHeader";
+
+function CadastroEquipe() {
+  return (
+    <div id="cadastro-equipe">
+      <DefaultHeader />
+    </div>
+  );
+}
+
+export default CadastroEquipe;
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import LoginToken from "./pages/LoginToken";
 import TelaInicial from "./pages/TelaInicial";
 import CadastroUsuario from "./pages/CadastroUsuario";
 import ResetarSenha from "./pages/ResetarSenha/index";
+import CadastroEquipe from "./pages/CadastroEquipe/index";
 
 function ProjetoRoutes() {
   return (
@@ -13,9 +14,10 @@ function ProjetoRoutes() {
       <Routes>
         <Route path="/" element={<TelaInicial />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<CadastroUsuario />} />
         <Route path="/login-token" element={<LoginToken />} />
         <Route path="/resetar-senha" element={<ResetarSenha />} />
+        <Route path="/cadastro-equipe" element={<CadastroEquipe />} />
+        <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
       </Routes>
     </BrowserRouter>
   );
