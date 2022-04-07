a11c8b37757ce55d52c0b8e6d615c08b46e1357b
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-07 02:37:11 -0300

    Inclusão de token para GETs

--- src/components/ListaCardsCompeticoesAbertas/index.js
+++ src/components/ListaCardsCompeticoesAbertas/index.js
@@ -4,40 +4,48 @@ import api from "../../services/api";
 import CardCompeticao from "../CardCompeticao";
 
 import "./styles.css";
-import '../../assets/styles/global.css';
+import "../../assets/styles/global.css";
 
 import FiltrosContext from "../../utils/filtrosContext";
+import StoreContext from "../../store/context";
 
 function ListaCardsCompeticoesAbertas() {
   const filtros = useContext(FiltrosContext);
 
   const [cards, setCards] = useState([]);
-  
+
+  const { token } = useContext(StoreContext);
+
   useEffect(() => {
     const { nomeCompeticao, mes, ano } = filtros;
 
-    const params = {}
-    if(nomeCompeticao !== '') {
+    const params = {};
+    if (nomeCompeticao !== "") {
       params.nomeCompeticao = nomeCompeticao;
     }
-    if(mes !== 0 && mes !== '') {
+    if (mes !== 0 && mes !== "") {
       params.mes = mes;
     }
-    if(ano !== 0 && ano !== '') {
+    if (ano !== 0 && ano !== "") {
       params.ano = ano;
     }
 
-    api.get("/competicoes/inscricoes", {params, }).then((response) => {
+    api.defaults.headers.post["Authorization"] = `Bearer ${token}`;
+    api.get("/competicoes/inscricoes", { params: filtros }).then((response) => {
       const { data } = response;
       setCards(data);
     });
-  }, [filtros]);
+  }, [filtros, token]);
 
   return (
     <div className="listagem-cards-competicoes">
       <ul>
         {cards.map((card) => {
-          return <li key={card.id}><CardCompeticao card={card} /></li>;
+          return (
+            <li key={card.id}>
+              <CardCompeticao card={card} />
+            </li>
+          );
         })}
       </ul>
     </div>
--- src/components/ListaCardsMinhasCompeticoes/index.js
+++ src/components/ListaCardsMinhasCompeticoes/index.js
@@ -4,49 +4,57 @@ import api from "../../services/api";
 import CardMinhasCompeticoes from "../CardMinhasCompeticoes";
 
 import "./styles.css";
-import '../../assets/styles/global.css';
+import "../../assets/styles/global.css";
 
 import FiltrosContext from "../../utils/filtrosContext";
+import StoreContext from "./../../store/context";
 
 function ListaCardsMinhasCompeticoes() {
   const filtros = useContext(FiltrosContext);
 
   const [cards, setCards] = useState([]);
 
+  const { token } = useContext(StoreContext);
+
   useEffect(() => {
     const { nomeCompeticao, mes, ano } = filtros;
 
-    const params = {}
-    if(nomeCompeticao !== '') {
+    const params = {};
+    if (nomeCompeticao !== "") {
       params.nomeCompeticao = nomeCompeticao;
     }
-    if(mes !== 0 && mes !== '') {
+    if (mes !== 0 && mes !== "") {
       params.mes = mes;
     }
-    if(ano !== 0 && ano !== '') {
+    if (ano !== 0 && ano !== "") {
       params.ano = ano;
     }
 
-    api.get("/competicoes/usuario-logado", {params, }).then((response) => {
+    api.defaults.headers.post["Authorization"] = `Bearer ${token}`;
+    api.get("/competicoes/usuario-logado", { params }).then((response) => {
       const { data } = response;
       setCards(data);
     });
-  }, [filtros]);
+  }, [filtros, token]);
 
   let papel = "";
   let cont = 0;
 
   function identificarPapelUsuario() {
     papel = cont % 2 === 0 ? "COMPETIDOR" : "ORGANIZADOR";
-    cont ++;
+    cont++;
   }
 
   return (
     <div className="listagem-cards-competicoes">
       <ul>
         {cards.map((card) => {
-          identificarPapelUsuario()
-          return <li><CardMinhasCompeticoes card={card} userRole={papel} /></li>;
+          identificarPapelUsuario();
+          return (
+            <li>
+              <CardMinhasCompeticoes card={card} userRole={papel} />
+            </li>
+          );
         })}
       </ul>
     </div>
