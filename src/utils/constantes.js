afea40e59123eb11472bb00e0ae18f880734affa
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-10 03:20:20 -0300

    Criada constante para padronizar modais

---
+++ src/utils/constantes.js
@@ -0,0 +1,10 @@
+export const styleModals = {
+  p: 4,
+  top: "50%",
+  left: "50%",
+  boxShadow: 24,
+  position: "absolute",
+  border: "2px solid #ffc107",
+  bgcolor: "background.paper",
+  transform: "translate(-50%, -50%)",
+};
