33df48bde1de209f01d38850a58e61471216d3f5
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-19 23:44:04 -0300

    Create usuarioLogadoContext.js

---
+++ src/utils/usuarioLogadoContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UsuarioLogadoContext = React.createContext({});
+
+export const UsuarioLogadoProvider = UsuarioLogadoContext.Provider;
+
+export default UsuarioLogadoContext;
