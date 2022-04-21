5a9aedc4f48f51fba82c339a524e70e14822bb4e
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-20 23:12:50 -0300

    Incluído tipo de mensagem LOADING

--- src/components/Mensagem/index.js
+++ src/components/Mensagem/index.js
@@ -1,18 +1,37 @@
 import React from "react";
-import { MSG005 } from "../../utils/mensagens";
-
 import "./styles.css";
 
+import { MSG005, MSG006, MSG008 } from "./../../utils/mensagens";
+
+import { Spinner } from "loading-animations-react";
+
 function Mensagem(props) {
+  const handleTitulo = () => {
+    let titulo;
+    let tipoMensagem = props.tipoMensagem;
+
+    if (tipoMensagem === MSG005) {
+      titulo = <h5>Sucesso!</h5>;
+    } else if (tipoMensagem === MSG006) {
+      titulo = <h5>Houve um erro!</h5>;
+    } else if (tipoMensagem === MSG008) {
+      titulo = <h5>Aguarde...</h5>;
+    }
+
+    return titulo;
+  };
+
   return (
     <div className={props.tipoMensagem + " mensagem"}>
       <div id="titulo" className="elementos-centralizados">
-        {props.tipoMensagem === MSG005 ? (
-          <h5>Sucesso!</h5>
-        ) : (
-          <h5>Houve um erro!</h5>
-        )}
+        {handleTitulo()}
       </div>
+      {props.tipoMensagem === MSG008 ? (
+        <div className="elementos-centralizados">
+          <Spinner color1="gray" color2="#fff" />
+        </div>
+      ) : null}
+
       <div className="elementos-centralizados">
         <p id="mensagem-texto">{props.mensagem}</p>
       </div>
