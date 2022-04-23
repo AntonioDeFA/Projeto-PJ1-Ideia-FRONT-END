e76fe24412e91f6eeb84f6b47a43169a31fb719f
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-04-23 02:09:17 -0300

    Ajustes no card de competição para incrição

--- src/components/CardCompeticao/index.js
+++ src/components/CardCompeticao/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../../assets/styles/global.css";
+import Botao from "../Botao";
 import "./styles.css";
 
 function CardCompeticao(props) {
   let dataInicio = props.card.etapaVigente.dataInicio;
   let dataTermino = props.card.etapaVigente.dataTermino;
+
+  const handleEquipesInscritas = () => {
+    let totalEquipesInscritas = props.card.quantidadeDeEquipes;
+    if (totalEquipesInscritas === 0) {
+      return "Não há equipes inscritas";
+    } else if (totalEquipesInscritas === 1) {
+      return "1 equipe inscrita";
+    } else {
+      return `${totalEquipesInscritas} equipes inscritas`;
+    }
+  };
 
   return (
     <div
@@ -15,22 +28,42 @@ function CardCompeticao(props) {
       <div className="card border border-warning tamanho-card-personalizado">
         <div className="card-header bg-warning">
           <h5 className="card-title fw-bold">{props.card.nomeCompeticao}</h5>
-          <h6 className="card-subtitle">
-            {dataInicio[2]}/{dataInicio[1]}/{dataInicio[0]} - {dataTermino[2]}/
-            {dataTermino[1]}/{dataTermino[0]}
-          </h6>
+          <div
+            className={
+              props.card.nomeCompeticao.length <= 15
+                ? "datas-etapa-bottom"
+                : null
+            }
+          >
+            <h6 className="card-subtitle">
+              {dataInicio[2]}/{dataInicio[1]}/{dataInicio[0]} - {dataTermino[2]}
+              /{dataTermino[1]}/{dataTermino[0]}
+            </h6>
+          </div>
         </div>
         <div className="card-body">
           <div className="text-center">
-            <p>
-              Para quem possui o domínio <br /> {props.card.dominioCompeticao}
-            </p>
-            <p>3 equipes inscritas</p>
+            {props.card.dominioCompeticao ? (
+              <p>
+                Para quem possui o domínio <br /> {props.card.dominioCompeticao}
+              </p>
+            ) : (
+              <p>Não há domínio especificado</p>
+            )}
+
+            <p>{handleEquipesInscritas()}</p>
           </div>
-          <div className="text-end">
-            <button className="btn btn-warning botao-menor-personalizado">
-              inscrever
-            </button>
+          <div
+            className={
+              props.card.dominioCompeticao ? "text-end" : "text-end pt-4"
+            }
+          >
+            <Link to={`/cadastro-equipe/${props.card.id}`}>
+              <Botao
+                titulo="inscrever"
+                classes="btn btn-warning botao-menor-personalizado"
+              />
+            </Link>
           </div>
         </div>
       </div>
