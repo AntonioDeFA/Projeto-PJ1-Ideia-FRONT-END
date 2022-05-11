927ea31082e0068d6ebd4015a8e26b9c4c23d5c6
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-05-11 02:52:23 -0300

    Create idCompeticaoContext.js

---
+++ src/utils/context/idCompeticaoContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IdCompeticaoContext = React.createContext({});
+
+export const IdCompeticaoProvider = IdCompeticaoContext.Provider;
+
+export default IdCompeticaoContext;
