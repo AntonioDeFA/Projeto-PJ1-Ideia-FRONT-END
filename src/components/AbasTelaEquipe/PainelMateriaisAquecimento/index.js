d45588c718157cd0dcdf6d2f80e271caf9e325bc
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-06-08 09:32:32 -0300

    Atualizando aba aquecimento

--- src/components/AbasTelaEquipe/PainelMateriaisAquecimento/index.js
+++ src/components/AbasTelaEquipe/PainelMateriaisAquecimento/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import {
   Box,
   List,
@@ -13,54 +13,44 @@ import {
   FormControlLabel
 } from "@mui/material";
 
+import api from "../../../services/api";
+import StoreContext from "../../../store/context";
+
 
 import "./styles.css";
 
 function PainelMateriaisAquecimento(props) {
 
-  //${props?.id} - usar isso aqui na chamada da api
+  const [materiais, setMateriais] = useState([]);
 
-  let materiais = [
-    {
-      id: 3,
-      nome: "material.pdf",
-      tipo: "PDF",
-      tempo: 120
-    },
-    {
-      id: 4,
-      nome: "material.mp4",
-      tipo: "VIDEO",
-      tempo: 120
-    },
-    {
-      id: 5,
-      nome: "material.link",
-      tipo: "LINK",
-      tempo: 120
-    },
-    {
-      id: 6,
-      nome: "eu",
-      tipo: "LINK",
-      tempo: 120
-    },
-    {
-      id: 7,
-      nome: "eu",
-      tipo: "LINK",
-      tempo: 120
-    },
-    {
-      id: 8,
-      nome: "eu",
-      tipo: "LINK",
-      tempo: 120
+  const { token } = useContext(StoreContext);
+
+  useEffect(() => {
+    api.defaults.headers.get["Authorization"] = `Bearer ${token}`;
+    api.get(`/${props?.id}/materiais-estudo`).then((response) => {
+      setMateriais(response.data);
+    });
+  }, []);
+
+  const baixarMaterial = (material, tipo) => {
+
+    if (tipo === "PDF") {
+      var byteCharacters = window.atob(material);
+      var byteNumbers = new Array(byteCharacters.length);
+      for (var i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+      var byteArray = new Uint8Array(byteNumbers);
+      var file = new Blob([byteArray], { type: "application/pdf;base64" });
+      var fileURL = URL.createObjectURL(file);
+      window.open(fileURL);
+    } else {
+      //aqui baixa vídeo
     }
-  ]
+  }
 
-  const baixarMaterial = (index) => {
-    console.log("Baixando material");
+  const abrirLink = (link) => {
+    window.open(link);
   }
 
   const IconeMaterial = (props) => {
@@ -72,18 +62,29 @@ function PainelMateriaisAquecimento(props) {
     } else if (tipo === "VIDEO") {
       return <i className="fa fa-file-video-o fa-2x me-3 mt-4"></i>;
     } else {
-      return <i className="fa fa-globe fa-2x me-3 mt-4"></i>;
+      return <i className="fa fa-globe fa-xl me-3 mt-4 pt-3"></i>;
     }
   }
 
   const IconeExecutavel = (props) => {
 
-    const { tipo, index } = props;
+    const { tipo, material, link } = props;
 
     if (tipo === "LINK") {
-      return <i className="fa fa-external-link pt-1 mt-4"></i>;
+      return (
+        <div className=" pt-1 mt-4">
+          <i className="fa fa-external-link icone-download-e-link-material"
+            onClick={() => abrirLink(link)}></i>
+        </div>
+      );
     } else {
-      return <i className="fa-solid fa-download pt-1 mt-4" onClick={() => baixarMaterial(index)}></i>;
+      return (
+        <div className=" pt-1 mt-4">
+          <i className="fa-solid fa-download icone-download-e-link-material"
+            onClick={() => baixarMaterial(material, tipo)}>
+          </i>
+        </div>
+      );
     }
 
   }
@@ -111,16 +112,20 @@ function PainelMateriaisAquecimento(props) {
               <ListItem key={material.id} >
                 <div className="d-flex justify-content-between w-100 pe-3 ps-3">
                   <div className="d-flex justify-content-center">
-                    <IconeMaterial tipo={material.tipo} />
+                    <IconeMaterial tipo={material.tipoMaterialEstudo} />
                     <div className="ms-3">
-                      <h6 className="mt-2">{material.nome}</h6>
+                      <h6 className="mt-2">{"material.nome"}</h6>
                       <div className="d-flex justify-content-center">
                         <FormControlLabel control={<Checkbox defaultChecked={false} />} label="concluído" />
                         <h6 className="mt-1 pt-2">{material.tempo} min</h6>
                       </div>
                     </div>
                   </div>
-                  <IconeExecutavel tipo={material.tipo} index={index} />
+                  <IconeExecutavel
+                    tipo={material.tipoMaterialEstudo}
+                    material={material.arquivoEstudo}
+                    link={material.link}
+                  />
                 </div>
               </ListItem>
             </ul>
