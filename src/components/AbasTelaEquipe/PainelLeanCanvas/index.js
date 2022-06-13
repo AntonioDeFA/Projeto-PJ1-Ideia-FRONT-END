321e3f1786d8702e67a4d79a52397cfcd604ffde
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-06-13 13:22:37 -0300

    Atualização Painel Lean Canvas

--- src/components/AbasTelaEquipe/PainelLeanCanvas/index.js
+++ src/components/AbasTelaEquipe/PainelLeanCanvas/index.js
@@ -1,9 +1,46 @@
 import React from "react";
 
+import Botao from "../../Botao/index";
+import LeanCanvas from "../../LeanCanvas/index";
+
 import "./styles.css";
 
 function PainelLeanCanvas(props) {
-  return <div id="painel-lean-canvas"></div>;
+
+
+  const salvar = () => {
+    console.log("salvar");
+  }
+
+  const enviarParaConsultoria = () => {
+    console.log("enviando para consultoria");
+  }
+
+  return (
+    <div id="painel-lean-canvas">
+      <h5 className="mb-4">Olá competidor, aqui você deverá editar seu Lean Canvas para que ele possa ser avaliado</h5>
+      <div className="d-flex justify-content-end">
+        <Botao
+          titulo="versões"
+          classes="btn btn-warning botao-menor-personalizado me-3"
+          onClick={null}
+        />
+        <Botao
+          titulo="salvar"
+          classes="btn btn-warning botao-menor-personalizado me-3"
+          onClick={() => salvar()}
+        />
+        <Botao
+          titulo="enviar para consultoria"
+          classes="btn btn-warning botao-menor-personalizado"
+          onClick={() => enviarParaConsultoria()}
+        />
+      </div>
+      <div className="mt-4">
+        <LeanCanvas />
+      </div>
+    </div>
+  );
 }
 
 export default PainelLeanCanvas;
