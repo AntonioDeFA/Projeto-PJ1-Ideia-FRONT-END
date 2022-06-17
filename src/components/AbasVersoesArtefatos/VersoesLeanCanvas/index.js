47a5247460c83a9917108a9a7df03eeac6a93e56
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-06-17 00:38:30 -0300

    Implementados componentes-aba de versões

---
+++ src/components/AbasVersoesArtefatos/VersoesLeanCanvas/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+function VersoesLeanCanvas(props) {
+  return (
+    <div id="versoes-lean-canvas">
+      <h1>Versões Lean Canvas</h1>
+    </div>
+  );
+}
+
+export default VersoesLeanCanvas;
---
+++ src/components/AbasVersoesArtefatos/VersoesPitchDeck/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+function VersoesPitchDeck(props) {
+  return (
+    <div id="versoes-pitch-deck">
+      <h1>Versões Pitch Deck</h1>
+    </div>
+  );
+}
+
+export default VersoesPitchDeck;
