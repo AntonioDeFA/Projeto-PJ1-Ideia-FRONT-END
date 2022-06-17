5ca3c2b810822329663506319343abaeec487621
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-06-17 18:53:09 -0300

    Atualizando aba de aquecimento

--- src/components/AbasTelaEquipe/PainelMateriaisAquecimento/index.js
+++ src/components/AbasTelaEquipe/PainelMateriaisAquecimento/index.js
@@ -55,16 +55,26 @@ function PainelMateriaisAquecimento(props) {
     window.open(link);
   }
 
+  const marcarConcluido = () => {
+    console.log("Marcado como concluído")
+  }
+
   const IconeMaterial = (props) => {
 
     const { tipo } = props;
 
     if (tipo === "PDF") {
-      return <i className="fa fa-file-pdf-o fa-2x me-3 mt-4 color-icone"></i>;
+      return (
+        <i className="fa fa-file-pdf-o fa-4x color-icone"></i>
+      );
     } else if (tipo === "VIDEO") {
-      return <i className="fa fa-file-video-o fa-2x me-3 mt-4 color-icone"></i>;
+      return (
+        <i className="fa fa-file-video-o fa-4x color-icone"></i>
+      );
     } else {
-      return <i className="fa fa-globe fa-xl me-3 mt-4 pt-3 color-icone"></i>;
+      return (
+        <i className="fa fa-globe fa-3x color-icone"></i>
+      );
     }
   }
 
@@ -75,14 +85,14 @@ function PainelMateriaisAquecimento(props) {
     if (tipo === "LINK") {
       return (
         <div className=" pt-1 mt-4">
-          <i className="fa fa-external-link icone-download-e-link-material"
+          <i className="fa fa-external-link icone-download-e-link-material cursor-pointer"
             onClick={() => abrirLink(link)}></i>
         </div>
       );
     } else {
       return (
         <div className=" pt-1 mt-4">
-          <i className="fa-solid fa-download icone-download-e-link-material"
+          <i className="fa-solid fa-download icone-download-e-link-material cursor-pointer"
             onClick={() => baixarMaterial(material, tipo)}>
           </i>
         </div>
@@ -110,15 +120,26 @@ function PainelMateriaisAquecimento(props) {
             key={index}
             className="pe-3"
           >
-            <ul onClick={null} className="border border-dark rounded mb-3">
+            <ul onClick={null} className="borda-laranja rounded mb-3">
               <ListItem key={material.id} >
                 <div className="d-flex justify-content-between w-100 pe-3 ps-3">
                   <div className="d-flex justify-content-center">
-                    <IconeMaterial tipo={material.tipoMaterialEstudo} />
+                    <div className="d-flex justify-content-center align-self-center me-2">
+                      <IconeMaterial tipo={material.tipoMaterialEstudo} />
+                    </div>
                     <div className="ms-3">
                       <h6 className="mt-2">{material.nome}</h6>
                       <div className="d-flex justify-content-start">
-                        <FormControlLabel control={<Checkbox defaultChecked={false} />} label="concluído" />
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              className="btn-check btn-outline-warning"
+                              defaultChecked={false}
+                              onChange={() => marcarConcluido()}
+                            />
+                          }
+                          label="concluído"
+                        />
                       </div>
                     </div>
                   </div>
