b1cbe1cffa5a7c7a360f85dd5e29cb0166bcfad7
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2022-07-14 03:21:15 -0300

    Atualização aba versões pitch deck

--- src/components/AbasVersoesArtefatos/VersoesPitchDeck/index.js
+++ src/components/AbasVersoesArtefatos/VersoesPitchDeck/index.js
@@ -3,215 +3,160 @@ import React, { useContext, useEffect, useState } from "react";
 import { List, TextareaAutosize } from "@mui/material";
 
 import Botao from "../../Botao";
+import api from "../../../services/api";
+import StoreContext from "../../../store/context";
+import { handleDataComHora } from "../../../services/utils";
 
 import "./styles.css";
 
 function VersoesPitchDeck(props) {
 
+  const { token } = useContext(StoreContext);
+
   const [mudouFeedBacks, setMudouFeedbacks] = useState(true);
+  const [mudouBotao, setMudouBotao] = useState(true);
+  const [habilitar, setHabilitar] = useState(true);
+
+  const [versoes, setVersoes] = useState([]);
   const [feedbacks, setFeedbacks] = useState([]);
   const [feedbacksPotencialidades, setFeedbacksPotencialidades] = useState([]);
   const [feedbacksFragilidades, setFeedbacksFragilidades] = useState([]);
 
   const escolherVersao = async (listaFeedbacks) => {
-    console.log("ME CHAMOU")
-    console.log("listaFeedbacks")
-    console.log(listaFeedbacks)
-    if (feedbacksPotencialidades.length > 0 && feedbacksFragilidades.length > 0) {
-      setFeedbacksPotencialidades([]);
-      setFeedbacksFragilidades([]);
-    }
+    feedbacks.splice(0, feedbacks.length);
+    feedbacksPotencialidades.splice(0, feedbacksPotencialidades.length);
+    feedbacksFragilidades.splice(0, feedbacksFragilidades.length);
 
     listaFeedbacks.forEach((feedback) => {
-      if (feedback.tipo === "POTENCIALIDADE") {
+      if (feedback.tipoFeedback === "POTENCIALIDADE") {
         feedbacksPotencialidades.push(feedback);
       } else {
         feedbacksFragilidades.push(feedback);
       }
     });
-    setFeedbacks(feedbacksPotencialidades);
 
     await setTimeout(() => {
-      setMudouFeedbacks(false);
-      setMudouFeedbacks(true);
-    }, 500);
-    console.log(feedbacksPotencialidades)
-    console.log(feedbacksFragilidades)
-  }
-
-  const trocarListaFeedbacks = async (tipo) => {
-    console.log("feedbacks")
-    console.log(feedbacks)
-    setFeedbacks([]);
-    if (tipo === "POTENCIALIDADES") {
       setFeedbacks(feedbacksPotencialidades);
-      console.log(feedbacksPotencialidades)
-    } else {
-      setFeedbacks(feedbacksFragilidades);
-      console.log(feedbacksFragilidades)
-    }
-
-    await setTimeout(() => {
+      setHabilitar(true);
       setMudouFeedbacks(false);
       setMudouFeedbacks(true);
+
     }, 400);
   }
 
-  const baixarArquivo = (arquivo) => {
+  const trocarListaFeedbacks = async (tipo) => {
+    if (tipo === "POTENCIALIDADES") {
+      setFeedbacks(feedbacksPotencialidades);
+      setHabilitar(true);
+    } else {
+      setFeedbacks(feedbacksFragilidades);
+      setHabilitar(false);
+    }
+
+    await setTimeout(() => {
+      setMudouFeedbacks(false);
+      setMudouFeedbacks(true);
+      setMudouBotao(false);
+      setMudouBotao(true);
+    }, 400);
+
+  }
+
+  const baixarArquivo = (arquivo, tipo) => {
+
+    let tipoArquivo = "video/mp4;base64";
+
+    if (tipo === "ARQUIVO") {
+      tipoArquivo = "application/pdf;base64";
+    }
+
+    var byteCharacters = window.atob(arquivo);
+    var byteNumbers = new Array(byteCharacters.length);
+    for (var i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    var byteArray = new Uint8Array(byteNumbers);
+    var file = new Blob([byteArray], { type: tipoArquivo });
+    var fileURL = URL.createObjectURL(file);
+    window.open(fileURL);
     console.log("Baixando arquivo");
   }
 
-  let versoes = [
-    {
-      data: "29/11/2021 - 23:12",
-      arquivo: "AAAAAAAAAAAAAAA",
-      feedbacks: [
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "praesentium quia."
-        },
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        }
-      ]
-    },
-    {
-      data: "29/11/2021 - 23:12",
-      arquivo: "AAAAAAAAAAAAAAA",
-      feedbacks: [
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        }
-      ]
-    },
-    {
-      data: "29/11/2021 - 23:12",
-      arquivo: "AAAAAAAAAAAAAAA",
-      feedbacks: [
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        }
-      ]
-    },
-    {
-      data: "29/11/2021 - 23:12",
-      arquivo: "AAAAAAAAAAAAAAA",
-      feedbacks: [
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "POTENCIALIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        },
-        {
-          tipo: "FRAGILIDADE",
-          descricao: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, officia, culpa tempore, vitae tenetur molestiae sint odio cumque aut voluptas earum commodi dolore nostrum nobis dolor nihil? Iusto, praesentium quia."
-        }
-      ]
-    }
-  ]
+  useEffect(() => {
+    api.defaults.headers.get["Authorization"] = `Bearer ${token}`;
+    api
+      .get(`/pitch-deck/${props.idEquipe}/feedbacks-de-versoes-consultoria`)
+      .then((response) => {
+        setVersoes(response.data)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+
+  }, []);
 
   return (
     <div id="versoes-pitch-deck">
       <div className="d-flex justify-content-between">
         <div className="w-50 cor-background-feedbacks p-3">
-          <div>
-            <Botao
+          {versoes.length !== 0 ? (<div>
+            {mudouBotao ? (<Botao
               titulo="potencialidades"
               onClick={() => trocarListaFeedbacks("POTENCIALIDADES")}
               id="id-btn-feedbacks-potencialidades"
-              classes="btn btn-warning botao-menor-personalizado"
-            />
-          </div>
-          <div className="mt-2">
-            <Botao
+              classes={habilitar ? "btn btn-secondary botao-menor-personalizado me-2" : "btn btn-warning botao-menor-personalizado me-2"}
+              disabled={habilitar}
+            />) : null}
+            {mudouBotao ? (<Botao
               titulo="fragilidades"
               onClick={() => trocarListaFeedbacks("FRAGILIDADES")}
               id="id-btn-feedbacks-fragilidades"
-              classes="btn btn-warning botao-menor-personalizado espacamento-botoes"
-            />
-          </div>
-          <div id="id-lista-feedbacks" className="mt-3">
-            <List
-              className="cor-background-feedbacks"
-              sx={{
-                width: "100%",
-                position: "relative",
-                overflow: "auto",
-                maxHeight: 368,
-                "& ul": { padding: 0 },
-              }}
-              subheader={<li />}
-            >
-              {feedbacks.map((feedback, index) => (
-                <li key={index} className="rounded mb-3 p-3 borda-laranja align-self-center cor-background-card">
-                  <div>
-                    <h6 className="mb-2">
-                      {feedback.tipo}
-                    </h6>
-                  </div>
-                  <div>
-                    <TextareaAutosize
-                      aria-label="minimum height"
-                      minRows={2}
-                      value={feedback.descricao}
-                      className="w-100 p-2 ps-0 cor-background-card border-0"
-                      style={{
-                        height: 150,
-                        resize: "none",
-                      }}
-                      disabled={true}
-                    />
-                  </div>
+              classes={habilitar ? "btn btn-warning botao-menor-personalizado me-2" : "btn btn-secondary botao-menor-personalizado me-2"}
+              disabled={!habilitar}
+            />) : null}
 
-                </li>
-              ))}
-            </List>
-          </div>
+            <div id="id-lista-feedbacks" className="mt-3">
+              <List
+                className="cor-background-feedbacks"
+                sx={{
+                  width: "100%",
+                  position: "relative",
+                  overflow: "auto",
+                  maxHeight: 320,
+                  "& ul": { padding: 0 },
+                }}
+                subheader={<li />}
+              >
+                {mudouFeedBacks ? feedbacks.map((feedback, index) => (
+                  <li key={index} className="rounded mb-3 p-3 borda-laranja align-self-center cor-background-card">
+                    <div>
+                      <h6 className="mb-2">
+                        {feedback.tipoFeedback}
+                      </h6>
+                    </div>
+                    <div>
+                      <TextareaAutosize
+                        aria-label="minimum height"
+                        minRows={2}
+                        value={feedback.sugestao}
+                        className="w-100 p-2 ps-0 cor-background-card border-0"
+                        style={{
+                          height: 120,
+                          resize: "none",
+                        }}
+                        disabled={true}
+                      />
+                    </div>
+
+                  </li>
+                )) : null}
+              </List>
+            </div>
+          </div>) : null}
         </div>
         <div className="w-50 p-3">
-          <List
+          {versoes.length !== 0 ? (<List
             sx={{
               width: "100%",
               bgcolor: "background.paper",
@@ -222,7 +167,7 @@ function VersoesPitchDeck(props) {
             }}
             subheader={<li />}
           >
-            {versoes.map((versao, index) => (
+            {versoes.listaVersoesPitch?.map((versao, index) => (
               <li key={index} className="rounded mb-3 pe-3 ps-3 borda-laranja align-self-center">
                 <ul>
                   <li
@@ -230,24 +175,28 @@ function VersoesPitchDeck(props) {
                   >
                     <i
                       className="fa fa-picture-o cursor-pointer"
-                      onClick={() => escolherVersao(versao.feedbacks)}
+                      onClick={() => escolherVersao(versao.feedbacksAvaliativos)}
                       style={{ color: "#fc7a00", fontSize: "20pt" }}
                     ></i>
 
                     <h6 style={{ margin: 0 }}>
-                      Data: {versao.data}
+                      Data: {handleDataComHora(versao.data)}
                     </h6>
                     <div className="align-self-center">
                       <i
                         className="fa-solid fa-download cursor-pointer"
-                        onClick={() => baixarArquivo(versao.arquivo)}
+                        onClick={() => baixarArquivo(versao.arquivoPitchDeck, versao.tipo)}
                       ></i>
                     </div>
                   </li>
                 </ul>
               </li>
             ))}
-          </List>
+          </List>) :
+            <div className="d-flex justify-content-center align-items-center borda-laranja rounded">
+              <h5 className="m-1">Não existe nenhuma versão de pitch avaliada pelo consultor.</h5>
+            </div>
+          }
         </div>
       </div>
     </div>
