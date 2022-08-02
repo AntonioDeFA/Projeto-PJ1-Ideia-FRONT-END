57c17862c6dc4a3e89862af99e4f2896d92b4952
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-08-02 02:20:20 -0300

    Mensagem de ausência de versões de feedbacks

--- src/components/AbasVersoesArtefatos/VersoesLeanCanvas/index.js
+++ src/components/AbasVersoesArtefatos/VersoesLeanCanvas/index.js
@@ -25,45 +25,51 @@ function VersoesLeanCanvas(props) {
 
   return (
     <div id="versoes-lean-canvas">
-      <List
-        sx={{
-          width: "100%",
-          bgcolor: "background.paper",
-          position: "relative",
-          overflow: "auto",
-          maxHeight: 420,
-          "& ul": { padding: 0 },
-        }}
-        subheader={<li />}
-      >
-        {versoes.map((versao, index) => (
-          <li key={index} className="rounded mb-3 p-2 borda-laranja">
-            <ul>
-              <li
-                className="d-flex justify-content-between align-items-center mt-2 mb-2 p-2 w-100"
-                id="versao-acesso-lista"
-              >
-                <i
-                  className="fa-brands fa-trello"
-                  style={{ color: "#fc7a00", fontSize: "20pt" }}
-                ></i>
-
-                <h6 style={{ margin: 0 }}>
-                  {formatarDataEHora(versao.dataHoraAprovacao)}
-                </h6>
-                <Link
-                  to={`/equipe/${props.idEquipe}/${props.papelUsuario}/feedbacks-lean-canvas/${versao.idLeanCanvas}`}
+      {versoes.length > 0 ? (
+        <List
+          sx={{
+            width: "100%",
+            bgcolor: "background.paper",
+            position: "relative",
+            overflow: "auto",
+            maxHeight: 420,
+            "& ul": { padding: 0 },
+          }}
+          subheader={<li />}
+        >
+          {versoes.map((versao, index) => (
+            <li key={index} className="rounded mb-3 p-2 borda-laranja">
+              <ul>
+                <li
+                  className="d-flex justify-content-between align-items-center mt-2 mb-2 p-2 w-100"
+                  id="versao-acesso-lista"
                 >
                   <i
-                    className="fa-solid fa-arrow-right-to-bracket hover-azul"
-                    style={{ fontSize: "20pt" }}
+                    className="fa-brands fa-trello"
+                    style={{ color: "#fc7a00", fontSize: "20pt" }}
                   ></i>
-                </Link>
-              </li>
-            </ul>
-          </li>
-        ))}
-      </List>
+
+                  <h6 style={{ margin: 0 }}>
+                    {formatarDataEHora(versao.dataHoraAprovacao)}
+                  </h6>
+                  <Link
+                    to={`/equipe/${props.idEquipe}/${props.papelUsuario}/feedbacks-lean-canvas/${versao.idLeanCanvas}`}
+                  >
+                    <i
+                      className="fa-solid fa-arrow-right-to-bracket hover-azul"
+                      style={{ fontSize: "20pt" }}
+                    ></i>
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          ))}
+        </List>
+      ) : (
+        <h5>
+          Não existe nenhuma versão de Lean Canvas avaliada pelo consultor.
+        </h5>
+      )}
     </div>
   );
 }
